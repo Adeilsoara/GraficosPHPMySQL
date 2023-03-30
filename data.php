41d203cf62e6e68c10c494bccc6bc06766650f75
Adeilsoara <adeilsonaragao@hotmail.com>
2023-03-30 14:56:51 -0300

    Comentários sobre o código

--- data.php
+++ data.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'connect.php';
-$stmt = $conn->prepare('select * from product');
+$stmt = $conn->prepare('select * from product'); //consulta responsável por preencher os gráficos
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_OBJ);
 echo json_encode($results);
